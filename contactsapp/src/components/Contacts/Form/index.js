90bb87aad4913fff18c23326ddd89118b179f9c7
Öykü Kaya <oykuky001@gmail.com>
2023-08-08 23:22:00 +0300

    form finished

--- contactsapp/src/components/Contacts/Form/index.js
+++ contactsapp/src/components/Contacts/Form/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {useState,useEffect} from "react"
 
-function Form(addContact) {
+function Form({addContact , contacts}) {
     const [form ,setForm] = useState({fullName: "" , phone_number: ""});
     //inputtaki değişiklikler                                               
     const onChangeInput = (e) => {
@@ -13,16 +13,16 @@ function Form(addContact) {
         if(form.fullName === "" || form.phone_number === "") {return false;}
        
         addContact([...contacts, form]); //array e eski verileri koruyarak atama yapıldı
-        console.log(form);
+        setForm({fullName : "", phone_number :""}) //form içini temizleme
     }
     return (
         <form onSubmit={onSubmit} >
         <div className="m-5">
            <div className="m-5">
-            <input  name="fullName" placeholder="Full Name" onChange={onChangeInput}/>
+            <input  name="fullName" placeholder="Full Name" value={form.fullName} onChange={onChangeInput} />
            </div>
            <div>
-           <input  name="phone_number" placeholder="Phone Number" onChange={onChangeInput} />
+           <input  name="phone_number" placeholder="Phone Number" value={form.phone_number} onChange={onChangeInput} />
            </div>
            <button className="m-3 btn btn-secondary"> ADD </button>
         </div>
