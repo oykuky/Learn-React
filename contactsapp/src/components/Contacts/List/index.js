c0f9535d8b09df2a175011be064f96eb74f54ab4
Öykü Kaya <oykuky001@gmail.com>
2023-08-10 00:27:37 +0300

    filtered added

--- contactsapp/src/components/Contacts/List/index.js
+++ contactsapp/src/components/Contacts/List/index.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import {useState} from 'react'
 import {ListGroup,ListGroupItem} from 'reactstrap'
 
 //PROPS SÜSLÜ PARANTEZLER İÇİNDE TANIMLA !!!!!!!!!
 function List({listofcontacts}) {
    // props 'u map ile kullanabilmek için bir array olmalı
-  if (!Array.isArray(listofcontacts)) {
-    return <div>Error: listofcontacts prop should be an array.</div>;
-}
+  const [filterText,setFilterText] = useState('')
+
+  const filtered = listofcontacts.filter((item) => {
+    return Object.keys(item).some((key) => 
+      item[key].toString().toLowerCase().includes(filterText.toLowerCase())
+     )
+  })
+  console.log(filtered)
   return (
     <div className='m-5'>
-        <h2>Contact List</h2>
-           
+        <h2 className='text-success'>Contact List</h2>
 
-        <ListGroup>
-          {listofcontacts.map((p , index) => (
-             <ListGroupItem key={index}>
-              <p> {p.fullName}</p>
-              <p> {p.phone_number}</p>
+        {/* filterText State'i: Bu state, kullanıcının filtreleme için yazdığı metni tutar
+         Kullanıcının girdisi her değiştiğinde,bu state güncellenir.
+        onChange Olayı: Kullanıcı herhangi bir giriş yaptığında, onChange olayı tetiklenir ve yeni değeri setFilterText işlevi ile filterText state'ine atanır. */}
+        <input className='m-5' placeholder='Filter Contact List' value={filterText} onChange={(e) => setFilterText(e.target.value)} />
+
+        <ListGroup className='m-5'>
+
+          {filtered.map((p , index) => (
+             <ListGroupItem key={index} className='text-primary'>
+              <p> Name : {p.fullName} <br/> Phone Number : {p.phone_number}</p>
              </ListGroupItem>
-
-          ))}
-         
+           ))}      
         </ListGroup>
 
     </div>
