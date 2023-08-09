10f84a72fc43fa8eab849ef6b8f19a4e29611ecd
Öykü Kaya <oykuky001@gmail.com>
2023-08-09 22:53:48 +0300

    list element added

--- contactsapp/src/components/Contacts/List/index.js
+++ contactsapp/src/components/Contacts/List/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {ListGroup,ListGroupItem} from 'reactstrap'
 
-function List() {
+//PROPS SÜSLÜ PARANTEZLER İÇİNDE TANIMLA !!!!!!!!!
+function List({listofcontacts}) {
+   // props 'u map ile kullanabilmek için bir array olmalı
+  if (!Array.isArray(listofcontacts)) {
+    return <div>Error: listofcontacts prop should be an array.</div>;
+}
   return (
     <div className='m-5'>
-        Contact List
+        <h2>Contact List</h2>
+           
+
+        <ListGroup>
+          {listofcontacts.map((p , index) => (
+             <ListGroupItem key={index}>
+              <p> {p.fullName}</p>
+              <p> {p.phone_number}</p>
+             </ListGroupItem>
+
+          ))}
+         
+        </ListGroup>
+
     </div>
   )
 }
