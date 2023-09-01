cebf5557986fd19154639bc63263b3d539ec9843
Öykü Kaya <oykuky001@gmail.com>
2023-09-01 23:09:49 +0300

    updated

--- react-to_do_list/src/App.js
+++ react-to_do_list/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './App.css';
-import {} from 'reactstrap'
 import Index from './components';
 
 function App() {
   return (
-    <div className="App">
+    <div>
     <Index/>
     </div>
   );
