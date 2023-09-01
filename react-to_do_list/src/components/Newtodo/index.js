20a3f14f1f957b3a0c3120e2d95c00b062c94cd4
Öykü Kaya <oykuky001@gmail.com>
2023-09-01 23:29:44 +0300

    todo filter added

--- react-to_do_list/src/components/Newtodo/index.js
+++ react-to_do_list/src/components/Newtodo/index.js
@@ -4,13 +4,41 @@ import { useState } from 'react'
 function Newtodo({todo ,setTodo}) {
 
    function DeleteClick (id){
-
+    
     }
 
 
-  return (
-    <div>
 
+
+  return (
+    <div class = "">
+        <ul class = "todo-list">
+
+        </ul>
+
+       <footer class="footer">
+		<span class="todo-count">
+			<strong>2</strong>
+			items left
+		</span>
+
+		<ul class="filters">
+			<li>
+				<a href="#/" class="selected">All</a>
+			</li>
+			<li>
+				<a href="#/">Active</a>
+			</li>
+			<li>
+				<a href="#/">Completed</a>
+			</li>
+		</ul>
+
+		<button class="clear-completed">
+			Clear completed
+		</button>
+	</footer>
+      
 
 
     </div>
