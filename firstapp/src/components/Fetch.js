7cbb9a0e37d6950a595c32b83428ba4c98358f8f
Öykü Kaya <oykuky001@gmail.com>
2023-09-04 15:23:22 +0300

    Loading state added

--- firstapp/src/components/Fetch.js
+++ firstapp/src/components/Fetch.js
@@ -6,15 +6,24 @@ function Fetch() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then((res) => res.json())
         .then((data)=>setUsers(data))
+        .finally(() =>setIsLoading(false))
     },[])
 
     const [users ,setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div className='mt-5'>USERS
       <div className='mt-3'>
+
       {
-            users.map((user) => <div key={user.id}>{user.name}</div>)
+            isLoading  && <div className='mb-2 text-warning'>Loading...</div>
+      }
+
+      {
+            users.map((user) => 
+            <div key={user.id}>{user.name}</div>
+            )
       }
 
       </div> 
