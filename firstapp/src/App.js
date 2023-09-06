6363f6efa5661e8d85d435375b4b5b449265cf92
Öykü Kaya <oykuky001@gmail.com>
2023-09-06 21:27:28 +0300

    router code added

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,105 +1,49 @@
 import './App.css';
 import {} from 'bootstrap';
-import {Container,Col,Row} from 'reactstrap';
-import { useState } from 'react';
-import Header  from './components/Header';
-import List2 from './components/List2';
-import List1 from './components/List1';
-import User from './components/User';
-import UseEffect from './components/UseEffect';
 import Fetch from './components/Fetch';
+import { BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Page from './components/Page';
+import Home from './components/Home';
 
-
-const nameSur = {
-  name: 'oyku' ,
-  surname: 'kaya'
-};
-
-const friends = [
-{ id: 1,
-  name : "deniz"
-},
-{ id: 2,
-  name : "merve "
-},
-{ id: 3,
-  name : "pınar "
-},
-];
 
 function App() {
-  const [name , setName] = useState("öykü");
-  const [surname , setSurname] = useState("Kaya");
-  const [newFriends, setNewFriends] = useState(['ayşe','mine','kasım']);
-  const [address, setAddress] = useState({title: 'İzmir' , zip : 2345});
-  const [isVisible,setIsVisible] = useState(true);
+  
   return (
     
-    <div>
-        <div className =  'text-center'>
-        <Fetch />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Fetch">Fetch</Link>
+              </li>
+              <li>
+                <Link to="/Page">Page</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+          <Route path="/" exact component={Home} />  
+          <Route path="/Fetch" element={<Fetch/>} />
+          <Route path="/Page" element={<Page/>} />
+          </Routes>
         </div>
-        
-       {/* state kullanımı */}
-      <nav className='m-5 text-center'>
-        <h2 className=" text-primary">{name} {surname}</h2>
-        <button className='m-5' onClick={() => setName("Kaya")}>Change the Name</button>
-        <button className='m-5' onClick={() => setSurname("Deniz")}>Change the Surname</button>
-      </nav>
-      <hr/>
-      <br></br>
-      <div className='text-center'>
-        {/* set edilen fonksiyonda array tipi olmalı ...array ile önceki array korunur ekleme işlemi yapılır */}
-        <button className='m-5' onClick={() => setNewFriends([...newFriends,'fatma'])}>Arkadaş Ekle</button> 
-        {
-          newFriends.map((p) =>  (<div className='m-2 text-center'>{p}</div>) )
-        }
+      </Router>
+     
+  );
 
-        {/* object state */}
-        <p className='m-5 text-primary'>{address.title} {address.zip} </p>
-        <button onClick={() => setAddress({...address, title : 'Ankara'})} >Change</button>
-      </div>
 
-      <Container>
-      <hr/>
-        <Row className='m-5 text-center'>
-        <Header/>
-            <h4>Benim adım {nameSur.name} soyadım {nameSur.surname}</h4>
-        </Row>
-        <hr/>
-        <Row className='m-4 text-center'>
-          <Col xs= "3">
-            <List1 title = "list1 "/>
-          </Col> 
-          <Col xs= "9">
-          <List2 title = "list2 "/>
-          </Col> 
-        </Row>
-
-        <Row className='text-center'>
-          <User
-          name = "Öykü"
-          isLoggedin = {true}
-          age = {22}
-          friends = {friends}
-          />
-
-        </Row>
-      </Container>
-      <Container>
-        {/* unmounted component */}
-        <Row className='m-5 text-danger'>
-          {/* isVisible true ise component gösterilir değilse gösterilmez */}
-          {isVisible &&  <UseEffect/>}
-          <button className='m-5 text-danger' onClick={() => setIsVisible(!isVisible)}>Toggle</button>
-          {/* toggle butonu ile componenti kaldırırız görünürlüğü değişir */}
-        </Row>
-       
-      </Container>
-        
-    </div>
-  )
 }
+
+
 
 export default App;
 
@@ -107,3 +51,4 @@ export default App;
 
 
 // rfce component kısayol
+
